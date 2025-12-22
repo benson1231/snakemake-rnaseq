1eb855d11df8fab925c30bcd865f607a76008d05
benson lee <benson106075@gmail.com>
2025-12-22 10:11:57 +0800

    feat: fix the typo

--- web/vite.config.js
+++ web/vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: process.env.NODE_ENV === 'production'
-    ? '/snakemake-rnaseq/'  // 👈 你的 GitHub repo 名稱
+    ? '/snakemake-rnaseq/'
     : '/',
 })
